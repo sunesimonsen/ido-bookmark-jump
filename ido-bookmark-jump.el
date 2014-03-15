ee85a4d3a2e46d593190c8112a3fa96d0e2a9974
Sune Simonsen <sune@we-knowhow.dk>
2014-03-15 12:36:24 +0100

    Initial commit

---
+++ ido-bookmark-jump.el
@@ -0,0 +1,8 @@
+(defun ido-bookmark-jump ()
+  "Uses ido to search for the bookmark"
+  (interactive)
+  (bookmark-jump
+   (bookmark-get-bookmark
+    (ido-completing-read "find bookmark: " (bookmark-all-names)))))
+
+(provide 'ido-bookmark-jump)
